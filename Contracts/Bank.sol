f486cfe5e622ae80ff73a8a7e47ccaa5c7d667ac
saikiran-13 <saikiran@simformsolutions.com>
2023-04-03 13:06:37 +0530

    Successfully created simple bank contract

--- Contracts/Bank.sol
+++ Contracts/Bank.sol
@@ -3,17 +3,27 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract simpleBank{
     mapping(address=>uint) public balances;
+    address public bankOwner;
 
     event etherDeposited(address,string);
     event etherWithdrawn(address,string);
 
+    constructor(){
+        bankOwner = payable(msg.sender);
+    }
+    
     modifier checkOwnerAddress(){
         require(msg.sender!=address(0),"Owner should not have zeroth address");
         _;
     }
 
+    modifier onlyBankOwner(){
+        require(msg.sender==bankOwner,"Only bankOwner can check the total contract balance");
+        _;
+    }
+
     //To check total contract Balance
-    function checkEther() public view checkOwnerAddress returns(uint){
+    function checkEther() public view checkOwnerAddress onlyBankOwner returns(uint){
         return address(this).balance;
     }
 
